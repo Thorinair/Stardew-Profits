bc25039a6bf3edbf2a846d5febb2248fc0543763
ShanderB <s.hander8910@hotmail.com>
2024-05-01 09:56:00 -0300

    Initial commit, with everything workin. I intend to refactory few things, but the current state is working. We had a problem that we have a half second where the i18n fields are empty. Need to fix this.

--- js/main.js
+++ js/main.js
@@ -1613,3 +1613,49 @@ document.addEventListener('DOMContentLoaded', initial);
 document.addEventListener('click', function (event) {
 	if (event.target.id === 'reset') window.location = 'index.html';
 });
+
+/**
+ * Set up the i18n language when the page loads.
+ */
+function setLanguage(){
+	//Get the current selected language.
+	var language = document.getElementById('select_language').value;
+
+	//Define the i18n file.
+	var filePath = language + '.json';
+
+	fetch('i18n/' + filePath)
+		.then(response => response.json())
+		.then(data => {
+			for (var key in data) {
+				var element = document.getElementById(key);
+				if (element) {
+					element.innerText = data[key];
+				}
+			}
+		});
+}
+
+//TODO Move to the top of the code.
+(function init() {
+  fetch('i18n/language-list.json')
+  .then(response => response.json())
+  .then(data => {
+	//TODO This function get the current language file and fill all the fields.
+	//TODO This should be usefull if we intend to add the langue to URL.
+	 var select = document.getElementById('select_language');
+
+    select.innerHTML = '';
+
+    for (var i = 0; i < data.length; i++) {
+      var option = document.createElement('option');
+      option.value = data[i].language;
+      option.text = data[i].text;
+      option.id = 'language_' + i;
+      select.appendChild(option);
+    }
+
+	//TODO document.body.style.display = "block";
+	setLanguage();
+  })
+})()
